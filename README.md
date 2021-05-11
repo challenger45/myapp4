ce5b1dcd2ba6c2245747608371cc92156d1e43e8
kickscar <kickscar@gmail.com>
2021-05-11 20:28:27 +0900

    S3 업로드 구현

---
+++ README.md
@@ -0,0 +1,9 @@
+# Node Application on EC2 Instance for AWS CloudFormation Template Example 
+
+### AWS::CloudFormation::Init Metadata Configuration on aws-cfn-bootstrap
+
+#### 1. packages: yum install
+#### 2. sources: tarball download from github's repository
+#### 3. files: redhat SysV init scripts
+#### 4. commands: npm install
+#### 5. services: sysvinit enable and start
---
+++ controller/uploadController.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+var AWS = require("aws-sdk");
+const model = require('../model');
+
+module.exports = {
+    upload: async function(req, res) {
+        const url = await storePhoto(req.file);
+        await model.Photo.create({
+            filename: req.file.originalname,
+            url: url,
+            comment: req.body.comment
+        })
+        res.redirect('/');
+    }
+}
+
+const storePhoto = async function(file) {
+    try {
+        const content = fs.readFileSync(file.path);
+
+        const key = path.join(process.env.AWS_S3_BUKET_OBJECT_KEY_PREFIX, file.filename) + path.extname(file.originalname);
+        const params = {
+            Body: content,
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: key,
+            ACL: "public-read",
+            ContentLength: file.size,
+            ContentType: file.mimetype
+        };
+
+        const stored = await new AWS.S3({
+            "region": process.env.AWS_REGION
+        }).upload(params).promise();
+
+        fs.unlinkSync(file.path);
+
+        return stored.Location;
+    } catch (err) {
+        console.error(err)
+    }
+}
